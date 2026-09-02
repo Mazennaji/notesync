653213eaa97f46bdfaa10a27c2b62a851027c747
Mazennaji <mazen.naji.1@gmail.com>
2026-09-02 13:55:21 +0300

    feat: implement command handling for IPC requests in notesync-core

--- core/cmd/notesync-core/main.go
+++ core/cmd/notesync-core/main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
+	"log/slog"
 	"os"
+
+	"github.com/Mazennaji/notesync/core/internal/ipc"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("notesync-core: no command provided")
-		os.Exit(1)
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+	in, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fail(logger, "failed to read stdin: "+err.Error())
+		return
 	}
-	fmt.Printf("notesync-core: received command %q (not yet implemented)\n", os.Args[1])
+
+	var req ipc.Request
+	if err := json.Unmarshal(in, &req); err != nil {
+		fail(logger, "invalid request json: "+err.Error())
+		return
+	}
+
+	logger.Info("received command", "command", req.Command)
+
+	var resp ipc.Response
+	switch req.Command {
+	case "ping":
+		resp = ipc.Response{OK: true, Data: map[string]string{"message": "pong"}}
+	case "status":
+		resp = ipc.Response{OK: true, Data: map[string]any{"status": "not yet implemented"}}
+	default:
+		resp = ipc.Response{OK: false, Error: "unknown command: " + req.Command}
+	}
+
+	json.NewEncoder(os.Stdout).Encode(resp)
+}
+
+func fail(l *slog.Logger, msg string) {
+	l.Error(msg)
+	json.NewEncoder(os.Stdout).Encode(ipc.Response{OK: false, Error: msg})
 }
